12777ed10e888ee262129d452625e71a72a8994e
seoyoonOh <5happy1004kr@naver.com>
2022-11-08 19:51:30 +0900

    소셜 로그인

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,5 +1,5 @@
 import { authService } from 'fbase';
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
+import { createUserWithEmailAndPassword, GithubAuthProvider, GoogleAuthProvider, signInWithEmailAndPassword, signInWithPopup } from 'firebase/auth';
 import React, { useState } from "react";
 
 const Auth = () => {
@@ -30,6 +30,17 @@ const Auth = () => {
     }
   };
   const toggleAccount = () => setNewAccount(prev => !prev);
+  const onSocialClick = async (event) => {
+    const {target: {name}} = event;
+    let provider;
+    if (name === "google") {
+      provider = new GoogleAuthProvider();
+    } else if ( name === "github") {
+      provider = new GithubAuthProvider();
+    }
+    const data = await signInWithPopup(authService, provider);
+    console.log(data);
+  };
   return (
     <div>
       <form onSubmit={onSubmit}>
@@ -40,8 +51,8 @@ const Auth = () => {
       </form>
       <span onClick={toggleAccount}>{ newAccount ? "Sign in" : "Create Account" }</span>
       <div>
-        <button>Continue with Google</button>
-        <button>Continue with Github</button>
+        <button name="google" onClick={onSocialClick}>Continue with Google</button>
+        <button name="github" onClick={onSocialClick}>Continue with Github</button>
       </div>
     </div>
   );
