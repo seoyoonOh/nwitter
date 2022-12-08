69c2cf1e99b8826e66e3788e7c13b0f60a159017
seoyoonOh <5happy1004kr@naver.com>
2022-12-08 17:54:20 +0900

    ✨ Add delete and update nweet

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,3 +1,4 @@
+import Nweet from 'components/Nweet';
 import { dbService } from 'fbase';
 import { addDoc, collection, onSnapshot, orderBy, query } from 'firebase/firestore';
 import React, { useEffect, useState } from "react";
@@ -47,9 +48,7 @@ const Home = ({ userObj }) => {
       </form>
       <div>
         {nweets.map((nweet) => (
-          <div key={nweet.id}>
-            <h4>{nweet.text}</h4>
-          </div>
+          <Nweet key={nweet.id} nweetObj={nweet} isOwner={nweet.creatorId === userObj.uid}/>
         ))}
       </div>
     </div>
