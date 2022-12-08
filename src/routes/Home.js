02fb7ba709fc18611b3f67636fba980076f60a26
seoyoonOh <5happy1004kr@naver.com>
2022-12-08 16:24:01 +0900

    ✨ nweet 기능 추가

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,9 +1,22 @@
+import { dbService } from 'fbase';
+import { addDoc, collection } from 'firebase/firestore';
 import React, { useState } from "react";
 
-const Home =  () => {
+const Home = () => {
   const [nweet, setNweet] = useState("");
-  const onSubmit = (event) => {
+  const onSubmit = async (event) => {
     event.preventDefault();
+    try {
+      const docRef = await addDoc(collection(dbService, "nweets"), {
+        nweet,
+        createdAt: Date.now(),
+      });
+      console.log("Document written with ID: ", docRef.id);
+    } catch (error) {
+      console.error("Error adding document: ", error);
+    }
+
+    setNweet("");
   };
   const onChange = (event) => {
     const { target:{value} } = event;
