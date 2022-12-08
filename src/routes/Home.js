0b7367b68c1f5f5e8781c794a43c419ae8d46056
seoyoonOh <5happy1004kr@naver.com>
2022-12-08 16:07:31 +0900

    nweet form, database 설정

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,5 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Home =  () => <span>Home</span>;
-
+const Home =  () => {
+  const [nweet, setNweet] = useState("");
+  const onSubmit = (event) => {
+    event.preventDefault();
+  };
+  const onChange = (event) => {
+    const { target:{value} } = event;
+    setNweet(value);
+  };
+  return (
+    <div>
+      <form>
+        <input value={nweet} onChange={onChange} type="text" placeholder="What's on your mind?" maxLength={120} />
+        <input onClick={onSubmit} type="submit" value="Nweet" />
+      </form>
+    </div>
+  );
+};
 export default Home;
